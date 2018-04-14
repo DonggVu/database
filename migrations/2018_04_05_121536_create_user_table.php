81fabf4c27a56116c0c634cf48d9e324660bc3d7
Dongg <Vudong71197@gmail.com>
2018-04-15 02:31:33 +0700

    Data

---
+++ migrations/2018_04_05_121536_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('full_name');
+            $table->date('birthday');
+            $table->string('email');
+            $table->string('phone');
+            $table->float('wallet')->default(1000000);
+            $table->rememberToken();
+            $table->integer('level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
